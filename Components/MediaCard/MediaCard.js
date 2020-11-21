49a939ca7d1c42cff47d3319f805cb025c4a698b
Victor Ragojos <vhsvragojos@gmail.com>
2020-11-21 00:30:17 -0800

    MOD: update styles

--- Components/MediaCard/MediaCard.js
+++ Components/MediaCard/MediaCard.js
@@ -1,80 +1,94 @@
-import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import useMediaCardStyles from './MediaCard.style';
-import CardMedia from '@material-ui/core/CardMedia';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import CallMadeIcon from '@material-ui/icons/CallMade';
+import PropTypes from "prop-types";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import useMediaCardStyles from "./MediaCard.style";
+import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CallMadeIcon from "@material-ui/icons/CallMade";
 
-const Index = ({ image, title, link, subtitle, description, direction = 'left' }) => {
-	const styles = useMediaCardStyles({ direction });
+const Index = ({ image, title, link, subtitle, description }) => {
+  const styles = useMediaCardStyles();
 
-	return (
-		<Box width="100%" className={styles.container}>
-			<Grid item xs={12} md={8} className={styles.imageContainer}>
-				<CardMedia image={image} alt={title} title={title} className={styles.image} />
-			</Grid>
-			<Grid item xs={12} md={4} container direction="column" justify="center" className={styles.info}>
-				<Grid item container alignItems="center" justify="space-between">
-					<Grid item>
-						<Typography variant="h5" color="primary" className={styles.title}>
-							{title}
-						</Typography>
-					</Grid>
+  return (
+    <Box width="100%" className={styles.container}>
+      <Grid item xs={12} className={styles.imageContainer}>
+        <CardMedia
+          image={image}
+          alt={title}
+          title={title}
+          className={styles.image}
+        />
+      </Grid>
+      <Grid item xs={12} container direction="column" className={styles.info}>
+        <Grid
+          item
+          container
+          alignItems="center"
+          justify="space-between"
+          className={styles.titleContainer}
+        >
+          <Grid item className="titleGrid">
+            <Typography variant="h5" color="primary" className={styles.title}>
+              {title}
+            </Typography>
+          </Grid>
 
-					{link && (
-						<Grid item>
-							<IconButton
-								href={link}
-								component="a"
-								color="inherit"
-								target="_blank"
-								disableFocusRipple
-								rel="noopener noreferrer"
-							>
-								<CallMadeIcon fontSize="small" />
-							</IconButton>
-						</Grid>
-					)}
-				</Grid>
+          {link && (
+            <Grid item>
+              <IconButton
+                href={link}
+                component="a"
+                color="inherit"
+                target="_blank"
+                disableFocusRipple
+                rel="noopener noreferrer"
+                aria-label={`${title.toLowerCase()}-project-link`}
+              >
+                <CallMadeIcon fontSize="small" />
+              </IconButton>
+            </Grid>
+          )}
+        </Grid>
 
-				{subtitle && (
-					<Grid item>
-						<Typography color="textSecondary" gutterBottom variant="subtitle2">
-							{subtitle}
-						</Typography>
-					</Grid>
-				)}
+        {subtitle && (
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom variant="subtitle2">
+              {subtitle}
+            </Typography>
+          </Grid>
+        )}
 
-				{description &&
-					description.map((v, i) => {
-						const isString = typeof v === 'string';
-						const isElement = typeof v === 'object';
-						return (
-							<Grid item key={`${v}-${i}`}>
-								{isString && (
-									<Typography color="textPrimary" variant="subtitle2">
-										- {v}
-									</Typography>
-								)}
+        {description &&
+          description.map((v, i) => {
+            const isString = typeof v === "string";
+            const isElement = typeof v === "object";
+            return (
+              <Grid item key={`${v}-${i}`}>
+                {isString && (
+                  <Typography color="textPrimary" variant="subtitle2">
+                    - {v}
+                  </Typography>
+                )}
 
-								{isElement && v}
-							</Grid>
-						);
-					})}
-			</Grid>
-		</Box>
-	);
+                {isElement && v}
+              </Grid>
+            );
+          })}
+      </Grid>
+    </Box>
+  );
 };
 
 Index.propTypes = {
-	link: PropTypes.string,
-	subtitle: PropTypes.string,
-	image: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	direction: PropTypes.oneOf(['left', 'right']),
-	description: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.node])),
+  link: PropTypes.string,
+  subtitle: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  direction: PropTypes.oneOf(["left", "right"]),
+  description: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.node])
+  ),
 };
 
 export default Index;
--- Components/MediaCard/MediaCard.style.js
+++ Components/MediaCard/MediaCard.style.js
@@ -1,98 +1,92 @@
-import { fade } from '@material-ui/core';
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import { fade, lighten } from "@material-ui/core";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 export default makeStyles(
-	({ shape, spacing, shadows, palette, typography, transitions, breakpoints }) => ({
-		container: ({ direction }) => ({
-			height: '100%',
-			display: 'flex',
-			overflow: 'hidden',
-			borderRadius: shape.borderRadius * 2,
-			transition: transitions.create('all'),
-			flexDirection: direction === 'left' ? 'row' : 'row-reverse',
+  ({
+    shape,
+    spacing,
+    shadows,
+    palette,
+    typography,
+    transitions,
+    breakpoints,
+  }) => ({
+    container: {
+      height: "100%",
+      display: "flex",
+      boxShadow: shadows[6],
+      flexDirection: "column",
+      borderRadius: shape.borderRadius,
+      transition: transitions.create("all"),
 
-			[breakpoints.down('sm')]: {
-				flexDirection: 'column',
-			},
+      "&:hover": {
+        transform: "scale(0.99)"
+      },
 
-			[breakpoints.down('xs')]: {
-				padding: spacing(1),
-			},
-		}),
-		imageContainer: {
-			width: '100%',
-			height: '100%',
-			minHeight: 320,
-			display: 'flex',
-			position: 'relative',
-			borderRadius: shape.borderRadius * 2,
+      [breakpoints.down("xs")]: {
+        padding: spacing(1),
+      },
+    },
+    imageContainer: {
+      width: "100%",
+      height: "100%",
+      minHeight: 320,
+      display: "flex",
+      position: "relative",
+      borderRadius: shape.borderRadius,
+    },
+    image: {
+      opacity: 0.6,
+      width: "100%",
+      height: "100%",
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+      borderTopLeftRadius: shape.borderRadius,
+      borderTopRightRadius: shape.borderRadius,
+      transition: transitions.create("all", { duration: 400 }),
 
-			[breakpoints.down('xs')]: {
-				height: 240,
-			},
-		},
-		image: {
-			opacity: 0.8,
-			width: '100%',
-			height: '100%',
-			borderRadius: shape.borderRadius * 2,
-			transition: transitions.create('all', { duration: 400 }),
+      "&:before": {
+        content: '""',
+        width: "100%",
+        height: "100%",
+        display: "block",
+        position: "absolute",
+        mixBlendMode: "screen",
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        borderTopLeftRadius: shape.borderRadius,
+        borderTopRightRadius: shape.borderRadius,
+        backgroundColor: fade(palette.primary.main, 0.18),
+        transition: transitions.create("all", { duration: 180 }),
+      },
 
-			'&:before': {
-				content: '""',
-				width: '100%',
-				height: '100%',
-				display: 'block',
-				position: 'absolute',
-				mixBlendMode: 'screen',
-				borderRadius: shape.borderRadius * 2,
-				backgroundColor: fade(palette.primary.main, 0.3),
-				transition: transitions.create('all', { duration: 400 }),
+      "&:hover:before, &:hover": {
+        opacity: 1,
+        backgroundColor: "transparent",
+      },
+    },
+    info: {
+      margin: "auto",
+      padding: spacing(2),
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      backgroundColor: palette.background.paper,
+      borderBottomLeftRadius: shape.borderRadius,
+      borderBottomRightRadius: shape.borderRadius,
+    },
+    titleContainer: {
+      flexWrap: "nowrap",
 
-				[breakpoints.down('sm')]: {
-					borderBottomLeftRadius: 0,
-					borderBottomRightRadius: 0,
-					borderTopLeftRadius: shape.borderRadius * 2,
-					borderTopRightRadius: shape.borderRadius * 2,
-				},
-			},
-
-			'&:hover:before, &:hover': {
-				opacity: 1,
-				backgroundColor: 'transparent',
-			},
-
-			[breakpoints.down('sm')]: {
-				borderBottomLeftRadius: 0,
-				borderBottomRightRadius: 0,
-				borderTopLeftRadius: shape.borderRadius * 2,
-				borderTopRightRadius: shape.borderRadius * 2,
-			},
-		},
-		info: ({ direction }) => ({
-			margin: 'auto',
-			overflow: 'hidden',
-			padding: spacing(2),
-			boxShadow: shadows[3],
-			backgroundColor: palette.background.paper,
-			...(direction === 'left'
-				? { borderTopRightRadius: shape.borderRadius * 2, borderBottomRightRadius: shape.borderRadius * 2 }
-				: { borderTopLeftRadius: shape.borderRadius * 2, borderBottomLeftRadius: shape.borderRadius * 2 }),
-
-			[breakpoints.down('sm')]: {
-				borderTopLeftRadius: 0,
-				borderTopRightRadius: 0,
-				borderBottomLeftRadius: shape.borderRadius * 2,
-				borderBottomRightRadius: shape.borderRadius * 2,
-			},
-
-			[breakpoints.down('xs')]: {
-				// padding: spacing(1),
-			},
-		}),
-		title: {
-			fontWeight: typography.fontWeightBold,
-		},
-	}),
-	{ name: 'MediaCard' }
+      "& .titleGrid": {
+        overflow: "hidden",
+      },
+    },
+    title: {
+      overflow: "hidden",
+      whiteSpace: "nowrap",
+      textOverflow: "ellipsis",
+      fontWeight: typography.fontWeightBold,
+    },
+  }),
+  { name: "MediaCard" }
 );
