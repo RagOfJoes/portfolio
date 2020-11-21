f266a769474165a7efcfc2b53891a9e2bdb7ffd0
Victor Ragojos <vhsvragojos@gmail.com>
2020-11-21 00:30:07 -0800

    MOD: reformat

--- Components/Header/Drawer.js
+++ Components/Header/Drawer.js
@@ -1,108 +1,129 @@
-import { useState } from 'react';
-import { routes, socials } from 'lib/constants';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import useHeaderStyles from './Header.style';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import MenuIcon from '@material-ui/icons/Menu';
-import ListItem from '@material-ui/core/ListItem';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import { useState } from "react";
+import { routes, socials } from "lib/constants";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import useHeaderStyles from "./Header.style";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import MenuIcon from "@material-ui/icons/Menu";
+import ListItem from "@material-ui/core/ListItem";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
-export default (props) => {
-	const styles = useHeaderStyles();
-	const [drawerOpen, togggleDrawerOpen] = useState(false);
-	const container = typeof window !== undefined ? () => window.document.body : undefined;
+const SideDrawer = (props) => {
+  const styles = useHeaderStyles();
+  const [drawerOpen, togggleDrawerOpen] = useState(false);
+  const container =
+    typeof window !== undefined ? () => window.document.body : undefined;
 
-	return (
-		<>
-			<Grid item className={styles.burger}>
-				<IconButton color="inherit" onClick={() => togggleDrawerOpen(true)}>
-					<MenuIcon color="inherit" />
-				</IconButton>
-			</Grid>
-			<Hidden mdUp implementation="css">
-				<Drawer
-					anchor="right"
-					open={drawerOpen}
-					variant="temporary"
-					container={container}
-					onClose={() => togggleDrawerOpen(false)}
-					BackdropProps={{
-						invisible: true,
-					}}
-					PaperProps={{
-						className: styles.drawer,
-					}}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
-				>
-					<Typography variant="h6" className={styles.title}>
-						Menu
-					</Typography>
+  return (
+    <>
+      <Grid item className={styles.burger}>
+        <IconButton
+          color="inherit"
+          aria-label="nav-menu-icon"
+          onClick={() => togggleDrawerOpen(true)}
+        >
+          <MenuIcon color="inherit" />
+        </IconButton>
+      </Grid>
+      <Hidden mdUp implementation="css">
+        <Drawer
+          anchor="right"
+          open={drawerOpen}
+          variant="temporary"
+          container={container}
+          onClose={() => togggleDrawerOpen(false)}
+          BackdropProps={{
+            invisible: true,
+          }}
+          PaperProps={{
+            className: styles.drawer,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          <Typography variant="h6" className={styles.title}>
+            Menu
+          </Typography>
 
-					<List className={styles.list}>
-						{routes.map((r) => {
-							const { route, Icon, displayName } = r;
-							return (
-								<ListItem
-									button
-									href={route}
-									component="a"
-									key={displayName}
-									onClick={() => togggleDrawerOpen(false)}
-								>
-									<ListItemIcon>{Icon}</ListItemIcon>
-									<ListItemText
-										primary={displayName}
-										primaryTypographyProps={{
-											variant: 'subtitle2',
-											color: 'textSecondary',
-											className: styles.listText,
-										}}
-									/>
-								</ListItem>
-							);
-						})}
-					</List>
+          <List className={styles.list}>
+            {routes.map((r) => {
+              const { route, Icon, displayName } = r;
+              return (
+                <ListItem
+                  button
+                  href={route}
+                  component="a"
+                  key={displayName}
+                  onClick={() => togggleDrawerOpen(false)}
+                  aria-label={`${displayName.toLowerCase()}=nav-link`}
+                >
+                  <ListItemIcon>{Icon}</ListItemIcon>
+                  <ListItemText
+                    primary={displayName}
+                    primaryTypographyProps={{
+                      variant: "subtitle2",
+                      color: "textSecondary",
+                      className: styles.listText,
+                    }}
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
 
-					<Grid
-						container
-						spacing={2}
-						justify="center"
-						style={{ width: '100%', overflow: 'hidden', marginTop: 'auto', padding: '48px 8px' }}
-					>
-						<Grid xs={12} item container spacing={2} justify="center" alignItems="center">
-							{socials.map((social) => {
-								const { Icon, link } = social;
+          <Grid
+            container
+            spacing={2}
+            justify="center"
+            style={{
+              width: "100%",
+              overflow: "hidden",
+              marginTop: "auto",
+              padding: "48px 8px",
+            }}
+          >
+            <Grid
+              xs={12}
+              item
+              container
+              spacing={2}
+              justify="center"
+              alignItems="center"
+            >
+              {socials.map((social) => {
+                const { Icon, link, name } = social;
 
-								return (
-									<Grid item key={link}>
-										<IconButton
-											href={link}
-											component="a"
-											color="inherit"
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											{Icon}
-										</IconButton>
-									</Grid>
-								);
-							})}
-						</Grid>
-						<Grid item>
-							<Typography variant="subtitle2" color="textSecondary">
-								Created by Victor Ragojos
-							</Typography>
-						</Grid>
-					</Grid>
-				</Drawer>
-			</Hidden>
-		</>
-	);
+                return (
+                  <Grid item key={link}>
+                    <IconButton
+                      href={link}
+                      component="a"
+                      color="inherit"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={`${name.toLowerCase()}=nav-link`}
+                    >
+                      {Icon}
+                    </IconButton>
+                  </Grid>
+                );
+              })}
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle2" color="textSecondary">
+                Created by Victor Ragojos
+              </Typography>
+            </Grid>
+          </Grid>
+        </Drawer>
+      </Hidden>
+    </>
+  );
 };
+
+export default SideDrawer;
--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,71 +1,101 @@
-import Link from 'next/link';
-import Drawer from './Drawer';
-import { routes } from 'lib/constants';
-import { HideOnScroll } from './helpers';
-import Grid from '@material-ui/core/Grid';
-import useHeaderStyles from './Header.style';
-import Button from '@material-ui/core/Button';
-import Hidden from '@material-ui/core/Hidden';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Link from "next/link";
+import Drawer from "./Drawer";
+import Image from "next/image";
+import { routes } from "lib/constants";
+import { HideOnScroll } from "./helpers";
+import Grid from "@material-ui/core/Grid";
+import useHeaderStyles from "./Header.style";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
-export default function HideAppBar(props) {
-	const styles = useHeaderStyles();
+const Header = (props) => {
+  const styles = useHeaderStyles();
 
-	return (
-		<HideOnScroll {...props}>
-			<AppBar elevation={0} position="sticky" color="transparent" className={styles.container}>
-				<Toolbar component="nav" className={styles.wrapper}>
-					<Grid wrap="nowrap" container justify="space-between" alignItems="center">
-						<Grid item container spacing={2} alignItems="center">
-							<Grid item href="/" component="a">
-								<img
-									alt="Logo"
-									title="Logo"
-									src="/images/icon-512.png"
-									style={{ width: 24, height: 24, borderRadius: 4 }}
-								/>
-							</Grid>
-							<Hidden smDown implementation="css">
-								<Grid item>
-									<Typography
-										href="/"
-										color="textPrimary"
-										variant="body1"
-										component="a"
-										className={styles.name}
-									>
-										Victor Ragojos
-									</Typography>
-								</Grid>
-							</Hidden>
-						</Grid>
+  return (
+    <HideOnScroll {...props}>
+      <AppBar
+        elevation={0}
+        position="sticky"
+        color="transparent"
+        className={styles.container}
+      >
+        <Toolbar component="nav" className={styles.wrapper}>
+          <Grid
+            container
+            wrap="nowrap"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item container spacing={2} alignItems="center">
+              <Grid item href="/" component="a">
+                <Image
+                  alt="Logo"
+                  width={24}
+                  height={24}
+                  title="Logo"
+                  src="/images/icon-512.png"
+                  style={{ borderRadius: 4 }}
+                />
+              </Grid>
+              <Hidden smDown implementation="css">
+                <Grid item>
+                  <Typography
+                    href="/"
+                    color="textPrimary"
+                    variant="body1"
+                    component="a"
+                    className={styles.name}
+                  >
+                    Victor Ragojos
+                  </Typography>
+                </Grid>
+              </Hidden>
+            </Grid>
 
-						<Hidden xsDown implementation="css">
-							<Grid item container spacing={4} wrap="nowrap" justify="flex-end" alignItems="center">
-								{routes.map((r) => {
-									const { route, Icon, displayName } = r;
+            <Hidden xsDown implementation="css">
+              <Grid
+                item
+                container
+                spacing={4}
+                wrap="nowrap"
+                justify="flex-end"
+                alignItems="center"
+              >
+                {routes.map((r) => {
+                  const { route, Icon, displayName } = r;
 
-									return (
-										<Grid item key={displayName}>
-											<Link shallow replace scroll={false} href={route}>
-												<Button variant="text" startIcon={Icon} classes={{ label: styles.listText }}>
-													<Typography variant="subtitle2" style={{ display: 'inline-flex' }}>
-														{displayName}
-													</Typography>
-												</Button>
-											</Link>
-										</Grid>
-									);
-								})}
-							</Grid>
-						</Hidden>
+                  return (
+                    <Grid item key={displayName}>
+                      <Link shallow replace scroll={false} href={route}>
+                        <Button
+                          variant="text"
+                          startIcon={Icon}
+                          classes={{ label: styles.listText }}
+                          aria-label={`${displayName.toLowerCase()}=nav-link`}
+                        >
+                          <Typography
+                            variant="subtitle2"
+                            style={{ display: "inline-flex" }}
+                          >
+                            {displayName}
+                          </Typography>
+                        </Button>
+                      </Link>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Hidden>
 
-						<Drawer />
-					</Grid>
-				</Toolbar>
-			</AppBar>
-		</HideOnScroll>
-	);
-}
+            <Drawer />
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
+  );
+};
+
+export default Header;
